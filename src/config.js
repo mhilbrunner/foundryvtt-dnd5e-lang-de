ec7581fa49966f745d1737f745a36c988260ba04
Max Hilbrunner <m.hilbrunner@gmail.com>
2022-10-23 00:41:28 +0200

    Remove code for older Foundry/system versions

--- src/config.js
+++ src/config.js
@@ -1,28 +1,5 @@
 export default [
     {
-        name: 'overrideSkillSortAlpha',
-        onlyUntilSystemVersionIncluding: '1.2.4',
-        data: {
-            name: 'Fertigkeiten alphabetisch sortieren',
-            hint: 'Sortiert die Fertigkeitenliste alphabetisch.',
-            scope: 'client',
-            type: Boolean,
-            config: true,
-            default: true
-        },
-    },
-    {
-        name: 'oglOverrideSkillSortAlpha',
-        data: {
-            name: 'Fertigkeiten alphabetisch sortieren (5e OGL Character Sheet)',
-            hint: 'Sortiert die Fertigkeitenliste alphabetisch.',
-            scope: 'client',
-            type: Boolean,
-            config: true,
-            default: false
-        },
-    },
-    {
         name: 'enableCompendiumTranslation',
         data: {
             name: 'Kompendiuminhalte übersetzen',
@@ -67,18 +44,6 @@ export default [
             type: Boolean,
             config: true,
             default: true,
-            onChange: () => window.location.reload()
-        }
-    },
-    {
-        name: 'increaseSheetWidth',
-        data: {
-            name: 'Breitere Charakterbögen',
-            hint: 'Verbreitet die Charakterbögen. Kann bei abgeschnittenem Text helfen. (Bei Änderung wird Foundry neu geladen.)',
-            scope: 'client',
-            type: Boolean,
-            config: true,
-            default: false,
             onChange: () => window.location.reload()
         }
     },
